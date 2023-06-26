c94a6eb5dfdfffd0ee2fe5ada9f54e6becb7800d
Max <maxmwangikenya@gmail.com>
2023-06-27 00:49:37 +0300

    Merge remote changes

--- db/migrate/20230626204907_create_restaurant_pizzas.rb
+++ db/migrate/20230626204907_create_restaurant_pizzas.rb
@@ -2,8 +2,8 @@
 class CreateRestaurantPizzas < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurant_pizzas do |t|
-      t.belongs_to :restaurant, null: false, foreign_key: true
-      t.belongs_to :pizza, null: false, foreign_key: true
+      t.integer :restraurant_id
+      t.integer :pizza_id
       t.integer :price
 
       t.timestamps
